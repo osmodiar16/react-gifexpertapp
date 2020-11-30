34329da2e4b866273db9fefcce7396b90752b7f2
Oscar <osmodiar16@gmail.com>
2020-11-29 22:21:39 -0800

    Docs agregados

---
+++ docs/precache-manifest.4a46bdbd68d22d864b05665c49f949af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e90d0569ec3d9e48d87b3c1694a742d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fc1697adb045f969ab8",
+    "url": "/static/css/main.13b35c8d.chunk.css"
+  },
+  {
+    "revision": "a8fdc59b7ee94837decf",
+    "url": "/static/js/2.069d0b1b.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.069d0b1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fc1697adb045f969ab8",
+    "url": "/static/js/main.d4b6b5f3.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
